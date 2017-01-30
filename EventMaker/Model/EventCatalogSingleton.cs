223b0a16525695e56c51c82341e961d6c88c91df
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-01-30 13:53:33 +0100

    Vi addede nogle klasser og skrev lidt kode i dem.

---
+++ EventMaker/Model/EventCatalogSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Streaming.Adaptive;
+
+namespace EventMaker.Model
+{
+    class EventCatalogSingleton : ObservableCollection<Event>
+    {
+        public void AddEvent()
+        {
+            
+        }
+    }
+}
