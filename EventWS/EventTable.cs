13f1e3b521d413da659678e05ef75c21091e381a
Jonatan <Jonatan@DESKTOP-3SG699B>
2017-03-27 10:49:58 +0200

    Jeg har lavet en EventWS

---
+++ EventWS/EventTable.cs
@@ -0,0 +1,29 @@
+namespace EventWS
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("EventTable")]
+    public partial class EventTable
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
